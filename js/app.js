5e6ed21922dfe8bbf5796e2aff5d9281fb9e083e
glous09 <goyavaes@hotmail.es>
2018-05-23 00:45:05 -0500

    modificando colores y animaciones

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@
       $('.text-animation').removeClass('hidden');
     }, 500);
   })()
+
+ //Navbar
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
